c050a3785786d2176c4e29d041f5a4411de0bbb7
rspeicher <rspeicher@gmail.com>
2009-08-30 18:24:55 -0400

    Initial git commit; more core files to be added later

---
+++ JuggyCompare_Data.lua
@@ -0,0 +1,15 @@
+JuggyCompare_Data = {
+  ["updated"] = "August 30, 2009 00:00",
+
+  ["Tsigo"] = {
+    ["NAME"]     = "Tsigo",
+    ["RANK"]     = "Officer",
+    ["RAIDS_30"] = "100%",
+    ["RAIDS_90"] = "100%",
+    ["RAIDS_LT"] = "100%",
+    ["LASTLOOT"] = "2009-08-30",
+    ["LF"]       = "0.00",
+    ["SLF"]      = "0.00",
+    ["BISLF"]    = "0.00",
+  },
+}
---
+++ JuggyCompare_Nicks.lua
@@ -0,0 +1,8 @@
+JuggyCompare_Nicks = {
+    method      = "Baud",
+    pwon        = "Parawon",
+    pgon        = "Parawon",
+    kam         = "Tsigo",
+    kamien      = "Tsigo",
+    sadris      = "Zadrix",
+}
