e292e71ac49bbf378fce4ae99d4bce50ac6fac7e
rspeicher <rspeicher@gmail.com>
2009-08-30 20:21:40 -0400

    Distributed loot copying
    Allows an attendance-taker to be outside of the raid and still
    copy item names/IDs directly from any person running this mod
    whenever they loot a corpse. Broadcasts corpse and item info
    to other users over the GUILD channel when in a raid.

--- JuggyAttendance.lua
+++ JuggyAttendance.lua
@@ -2,24 +2,37 @@
 -- Locals
 -- ---------------------
 
+-- Debugging
+local dbg = false
+
 -- Only copy epic and legendary loots
-local loot_cutoff = 4
+local rarity_cutoff = 4
 
--- ---------------------
--- AddOn Declaration
--- ---------------------
+-- Only copy item level 200 or higher loots
+local level_cutoff = 200
 
-local mod = LibStub("AceAddon-3.0"):NewAddon("JuggyAttendance", "AceConsole-3.0")
+-- Stores data about broadcasted loots
+local loots = {}
 
-function mod:OnInitialize()
-	self:RegisterChatCommand('att', 'CopyAttendance')
-	self:RegisterChatCommand('lc',  'CopyLoot')
+-- Stores data about GUIDs we already copied loot for
+local copied = {}
+
+local UnitInRaid, GetTime = _G.UnitInRaid, _G.GetTime
+
+-- Clear a table's contents
+local function terase(t)
+	for i in pairs(t) do t[i] = nil end
 end
 
-function mod:OnEnable()
-end
-
-function mod:OnDisable()
+-- Check if a table contains a value
+local function tcontains(t, value)
+	for k,v in pairs(t) do
+		if v == value or string.lower(v) == string.lower(value) then
+			return true
+		end
+	end
+	
+	return false
 end
 
 -- Copied from Chatter's urlCopy, thanks!
@@ -51,6 +64,74 @@ StaticPopupDialogs["JuggyCopyDialog"] = {
 }
 
 -- ---------------------
+-- AddOn Declaration
+-- ---------------------
+
+local mod = LibStub("AceAddon-3.0"):NewAddon("JuggyAttendance", "AceConsole-3.0", "AceComm-3.0", "AceEvent-3.0")
+
+function mod:OnInitialize()
+	self:RegisterChatCommand('att', 'CopyAttendance')
+	self:RegisterChatCommand('lc',  'CopyLoot')
+	
+	self.prefix = "jcomp"
+end
+
+function mod:OnEnable()
+	self:RegisterComm(self.prefix)
+	self:RegisterEvent("LOOT_OPENED")
+end
+
+function mod:OnDisable()
+end
+
+-- ---------------------
+-- Distributed Looting
+-- ---------------------
+
+function mod:OnCommReceived(prefix, msg, distro, sender)
+	local guid, itemId = select(3, msg:find("(%w+) (%d+)"))
+	
+	-- A table for this GUID already exists, and it wasn't created by this person
+	-- Probably means multiple people looted the corpse and broadcasted it, so just 
+	-- ignore duplicates
+	if loots[guid] ~= nil and loots[guid].source ~= sender then return end
+	
+	-- If a copied entry for this GUID already exists, it means we copied it and don't 
+	-- need to create it again
+	if tcontains(copied, guid) then return end
+	
+	-- Table for this GUID doesn't exist, initialize it, giving credit to the 
+	-- first sender
+	if loots[guid] == nil then
+		loots[guid] = {
+			source = sender,
+			items  = {}
+		}
+	end
+	
+	table.insert(loots[guid].items, itemId)
+end
+
+function mod:LOOT_OPENED(event, arg1)
+	if not dbg and not UnitInRaid('player') then return end
+	
+	-- Get the GUID of the looted corpse so we can broadcast it and separate 
+	-- loot by corpse
+	local guid = UnitGUID('target')
+	
+	for i=1, GetNumLootItems() do
+		-- Get loot info for each loot slot, and broadcast its data if it's an
+		-- item we're interested in
+		local _, name, _, rarity = GetLootSlotInfo(i)
+		-- NOTE: We only do a rarity check here; doing a level check would require extra calls
+		if dbg or rarity >= rarity_cutoff then
+			local _, itemId = strsplit(":", GetLootSlotLink(i))
+			self:SendCommMessage(self.prefix, (guid .. " " .. itemId), "GUILD")
+		end
+	end
+end
+
+-- ---------------------
 -- Methods
 -- ---------------------
 
@@ -64,9 +145,7 @@ function mod:CopyAttendance(method)
 		str = str .. UnitName('raid' .. i) .. sep
 	end
 	
-	--str = "Tsigo\nBaud\nSebudai\nSouai\nDuskshadow"
-	
-	if (str ~= "") then
+	if str ~= "" then
 		if method == "1" then
 			currentText = string.gsub(str, "\n$", '')
 			StaticPopup_Show("JuggyCopyDialog")
@@ -82,22 +161,22 @@ function mod:CopyLoot(method)
 	
 	local str = ""
 	
-	for i=1, GetNumLootItems() do
-		local _, name, _, rarity = GetLootSlotInfo(i)
-
-		if (rarity >= loot_cutoff and not string.find(name, "^Emblem of") and not string.find(name, "^(Plans|Pattern)")) then
-			local _, itemId = strsplit(":", GetLootSlotLink(i))
-			if method == "1" then
+	for guid, vals in pairs(loots) do
+		for k, itemId in pairs(vals.items) do
+			local name, _, rarity, level = GetItemInfo(itemId)
+			-- Make sure the item is high enough quality AND level
+			if dbg or (rarity >= rarity_cutoff and level >= level_cutoff) then
 				str = str .. " - " .. name .. "|" .. itemId .. "\n"
-			else
-				str = str .. itemId .. "|"
 			end
 		end
+		
+		table.insert(copied, guid)
 	end
 	
-	--str = " - Item|12345\n - Item2|54321"
+	-- Clear out this loots table since we've got everything we need
+	terase(loots)
 	
-	if (str ~= "") then
+	if str ~= "" then
 		if method == "1" then
 			currentText = string.gsub(str, "\n$", '')
 			StaticPopup_Show("JuggyCopyDialog")
